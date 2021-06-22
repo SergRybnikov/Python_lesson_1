2a22158a0f60b33e36cc2cd5705baa8e440b994f
SergRybnikov <serg2853@tut.by>
2021-06-22 11:11:33 +0300

    удалил пробелы

--- Rybnikov_Sergey_dz_3.py
+++ Rybnikov_Sergey_dz_3.py
@@ -1,9 +1,9 @@
 num = int(input('Введите число до 20: '))
 if num ==1:
     print(num, 'процент')
-if (num >= 2)  and (num < 5):
+if (num >= 2) and (num < 5):
     print(num, 'процента')
-if (num >= 5)  and (num <= 20):
+if (num >= 5) and (num <= 20):
     print(num, 'процентов')
 elif num >= 21:
     print('Введено не правильное значение')
